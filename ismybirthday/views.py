095d036a70d40494d3bc537ec441b73c5e41b7b6
Chhabinath Sahoo <53556959+chhabinath@users.noreply.github.com>
2022-02-19 11:56:07 +0530

    First commit

---
+++ ismybirthday/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+import datetime
+# Create your views here.
+
+
+def index(request):
+    now = datetime.datetime.now()
+    return render(request,"index.html",{
+        "birthday": now.month == 6 and now.day == 15
+    })
